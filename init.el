2d65810cad1d6e1646e1a56a6700b8bf525c1be9
Eric Gorelik <egorelik93@users.noreply.github.com>
2025-11-21 13:47:58 -0500

    Confirmed that having both dap and dape prevents setting breakpoints with the mouse.
    Disabling dap module.

--- init.el
+++ init.el
@@ -93,7 +93,7 @@
        ;;biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
        debugger          ; FIXME stepping through code, to help you add bugs
-       dap               ; Old version of debugger that I copied, which still uses dap mode.
+       ;;dap               ; Old version of debugger that I copied, which still uses dap mode.
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
